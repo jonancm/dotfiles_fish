084bf475c3d04a9d640b1e04c84074e593b48fb7
Jonan CM <jonancm@users.noreply.github.com>
2023-03-19 13:29:56 +0100

    Add fisher plugins

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-SCRIPT_DIR=`dirname "${BASH_SOURCE}"`
+SCRIPT_DIR=`readlink -f $(dirname "${BASH_SOURCE}")`
 ln -sv ${SCRIPT_DIR}/conf.d/*.fish ${HOME}/.config/fish/conf.d/
+ln -sv ${SCRIPT_DIR}/fish_* ${HOME}/.config/fish/
