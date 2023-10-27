e5a7505ddb72600c4d30c840b31adde52f8d3d81
Jonan CM <jonancm@users.noreply.github.com>
2023-10-27 12:47:06 +0200

    Configure pyenv only if it's installed

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -1,4 +1,6 @@
 set -x PYENV_ROOT "$HOME/.pyenv"
 fish_add_path "$PYENV_ROOT/bin"
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
+if command -v pyenv &> /dev/null
+	status --is-interactive; and . (pyenv init -|psub)
+	status --is-interactive; and . (pyenv virtualenv-init -|psub)
+end
